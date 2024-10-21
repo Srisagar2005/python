e2b0da27fb9a398a9165096c678deeafc86918c5
Srisagar2005 <166827097+Srisagar2005@users.noreply.github.com>
2024-10-21 14:58:04 +0530

    Create basic_arthematic_operations of two numbers using functions.py

---
+++ basic_arthematic_operations of two numbers using functions.py
@@ -0,0 +1,8 @@
+def basic_arthematic_operations():
+	print('addition(a+b):',a+b)
+	print('subtraction(a-b):',a-b)
+	print('multiplication(a*b):',a*b)
+	print('division(a/b):',a/b)
+a=int(input("enter a :"))
+b=int(input('enter b :'))
+basic_arthematic_operations()
